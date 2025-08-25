69bad560a01d352bc75a5a5f888e3237ac8ee595
Sarah Svedenborg <sarah.svedenborg@soprasteria.com>
2025-08-25 21:03:21 +0200

    posthog on window

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -14,14 +14,17 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
             api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
             person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
             defaults: '2025-05-24',
-            disable_web_experiments: false
+            disable_web_experiments: false,
+            loaded: function (posthog) {
+                // Assign posthog to the window object here
+                (window as any).posthog = posthog;
+            }
         })
-
-        if (!(window as any)?.posthog) {
-            console.warn('PostHog must be added to the window object on this page, for this to work. This is normally done in the loaded callback of your posthog init code.')
-        } else {
-            (window as any)?.posthog.loadToolbar({ "action": "ph_authorize", "token": process.env.NEXT_PUBLIC_POSTHOG_TOKEN as string })
-        }
+        /*   if (!(window as any)?.posthog) {
+              console.warn('PostHog must be added to the window object on this page, for this to work. This is normally done in the loaded callback of your posthog init code.')
+          } else {
+              (window as any)?.posthog.loadToolbar({ "action": "ph_authorize", "token": process.env.NEXT_PUBLIC_POSTHOG_TOKEN as string })
+          } */
 
     }, [])
 
