cf9df4573805f5ecda9c4abcbbfab37dda2a4258
Sarah Svedenborg <sarah.svedenborg@soprasteria.com>
2025-08-26 10:12:59 +0200

    initialize posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -9,10 +9,22 @@ import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
+    /*   useEffect(() => {
+          posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+              api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+              person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+              defaults: '2025-05-24',
+              disable_web_experiments: false,
+              loaded: function (posthog) {
+                  // Assign posthog to the window object here
+                  (window as any).posthog = posthog;
+              }
+          })
+    */
+
+    if (typeof window !== 'undefined') {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
             api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
-            person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
             defaults: '2025-05-24',
             disable_web_experiments: false,
             loaded: function (posthog) {
@@ -20,13 +32,9 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
                 (window as any).posthog = posthog;
             }
         })
-        /*   if (!(window as any)?.posthog) {
-              console.warn('PostHog must be added to the window object on this page, for this to work. This is normally done in the loaded callback of your posthog init code.')
-          } else {
-              (window as any)?.posthog.loadToolbar({ "action": "ph_authorize", "token": process.env.NEXT_PUBLIC_POSTHOG_TOKEN as string })
-          } */
+    }
 
-    }, [])
+
 
 
 
