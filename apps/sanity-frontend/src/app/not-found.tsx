9c6b866d7b38bf327a664260f24860c033af9bf7
Sarah Svedenborg <sarah.svedenborg@soprasteria.com>
2025-09-05 14:18:24 +0200

    404 page

---
+++ apps/sanity-frontend/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Heading } from 'ui-lib';
+
+
+const NotFoundPage = () => {
+
+
+    return (
+        <Heading level="h1" >
+            404</Heading>
+    )
+};
+
+export default NotFoundPage;
