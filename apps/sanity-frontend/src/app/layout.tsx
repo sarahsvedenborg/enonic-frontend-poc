2efe22b50b79435c1bfd48b1a1abe6718350409a
Sarah Svedenborg <sarah.svedenborg@soprasteria.com>
2025-09-05 11:47:36 +0200

    banner

--- apps/sanity-frontend/src/app/layout.tsx
+++ apps/sanity-frontend/src/app/layout.tsx
@@ -1,14 +1,8 @@
-import type { Metadata } from 'next'
-import { Header } from 'ui-lib'
+import { Header, Banner } from 'ui-lib'
 /* import { Inter } from 'next/font/google' */
 import '../../globals.css'
 
 /* const inter = Inter({ subsets: ['latin'] }) */
-
-export const metadata: Metadata = {
-    title: 'Sanity Frontend',
-    description: 'A simple frontend for Sanity CMS',
-}
 
 export default function RootLayout({
     children,
@@ -19,6 +13,7 @@ export default function RootLayout({
         <html lang="en">
             <body >
                 <div >
+                    <Banner site="Sanity CMS" login="https://sanity.io/manage" />
                     <Header />
                     <main >
                         {children}
